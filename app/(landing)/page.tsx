9583399cc8363a9d8c384003f79bfd4c381f51bc
Andon Zeno <tonizeno5@gmail.com>
2023-11-12 20:37:41 +0100

    Feat - Clerk Authentication

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 function LandingPage() {
-    return <div>LandingPage</div>;
+    return (
+        <div>
+            <Link href="/sign-in">
+                <Button>Login</Button>
+            </Link>
+            <Link href="/sign-up">
+                <Button>Register</Button>
+            </Link>
+        </div>
+    );
 }
 
 export default LandingPage;
